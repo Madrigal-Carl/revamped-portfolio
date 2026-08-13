6cc6dbe7af0d1d7de13124c509bc9a024d91189a
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-08-14 05:10:31 +0800

    adjust skills

--- frontend/src/data/skills.js
+++ frontend/src/data/skills.js
@@ -1,11 +1,89 @@
-import { MdWeb, MdDns, MdLayers, MdSmartphone, MdStorage, MdBuild } from 'react-icons/md'
-import { SiHtml5, SiReact, SiCss, SiTailwindcss, SiJavascript, SiNodedotjs, SiPhp, SiLaravel, SiLivewire, SiExpress, SiFlutter, SiDart, SiMysql, SiMongodb, SiSqlite, SiSequelize, SiMongoose, SiGit, SiPostman, SiAndroidstudio, SiVercel, SiRender } from 'react-icons/si'
+import {
+  MdWeb,
+  MdDns,
+  MdLayers,
+  MdSmartphone,
+  MdStorage,
+  MdBuild,
+} from "react-icons/md";
+import {
+  SiHtml5,
+  SiReact,
+  SiCss,
+  SiTailwindcss,
+  SiJavascript,
+  SiNodedotjs,
+  SiPhp,
+  SiLaravel,
+  SiLivewire,
+  SiExpress,
+  SiFlutter,
+  SiDart,
+  SiMysql,
+  SiMongodb,
+  SiSqlite,
+  SiSequelize,
+  SiMongoose,
+  SiGit,
+  SiPostman,
+  SiAndroidstudio,
+  SiVercel,
+  SiRender,
+  SiDocker,
+} from "react-icons/si";
 
 export const skills = [
-  ['Frontend', MdWeb, [['ReactJS', SiReact, 'Intermediate'], ['HTML', SiHtml5, 'Proficient'], ['CSS', SiCss, 'Proficient'], ['TailwindCSS', SiTailwindcss, 'Advanced'], ['JavaScript', SiJavascript, 'Intermediate']]],
-  ['Backend', MdDns, [['NodeJS', SiNodedotjs, 'Intermediate'], ['PHP', SiPhp, 'Intermediate']]],
-  ['Frameworks', MdLayers, [['Laravel', SiLaravel, 'Intermediate'], ['Livewire', SiLivewire, 'Intermediate'], ['ExpressJS', SiExpress, 'Intermediate'], ['Flutter', SiFlutter, 'Basic']]],
-  ['Mobile', MdSmartphone, [['Dart', SiDart, 'Basic']]],
-  ['Database', MdStorage, [['MySQL', SiMysql, 'Advanced'], ['MongoDB', SiMongodb, 'Intermediate'], ['SQLite', SiSqlite, 'Basic'], ['Sequelize ORM', SiSequelize, 'Intermediate'], ['Mongoose', SiMongoose, 'Intermediate']]],
-  ['Tools & Deployment', MdBuild, [['Git & GitHub', SiGit, 'Proficient'], ['Postman', SiPostman, 'Proficient'], ['Android Studio', SiAndroidstudio, 'Basic'], ['Vercel', SiVercel, 'Basic'], ['Render', SiRender, 'Basic']]],
-]
+  [
+    "Frontend",
+    MdWeb,
+    [
+      ["ReactJS", SiReact, "Advanced"],
+      ["HTML", SiHtml5, "Proficient"],
+      ["CSS", SiCss, "Proficient"],
+      ["TailwindCSS", SiTailwindcss, "Advanced"],
+      ["JavaScript", SiJavascript, "Intermediate"],
+    ],
+  ],
+  [
+    "Backend",
+    MdDns,
+    [
+      ["NodeJS", SiNodedotjs, "Intermediate"],
+      ["PHP", SiPhp, "Intermediate"],
+    ],
+  ],
+  [
+    "Frameworks",
+    MdLayers,
+    [
+      ["Laravel", SiLaravel, "Advanced"],
+      ["Livewire", SiLivewire, "Intermediate"],
+      ["ExpressJS", SiExpress, "Advanced"],
+      ["Flutter", SiFlutter, "Intermediate"],
+    ],
+  ],
+  ["Mobile", MdSmartphone, [["Dart", SiDart, "Intermediate"]]],
+  [
+    "Database",
+    MdStorage,
+    [
+      ["MySQL", SiMysql, "Advanced"],
+      ["MongoDB", SiMongodb, "Intermediate"],
+      ["SQLite", SiSqlite, "Basic"],
+      ["Sequelize ORM", SiSequelize, "Intermediate"],
+      ["Mongoose", SiMongoose, "Intermediate"],
+    ],
+  ],
+  [
+    "Tools & Deployment",
+    MdBuild,
+    [
+      ["Git & GitHub", SiGit, "Proficient"],
+      ["Postman", SiPostman, "Proficient"],
+      ["Docker", SiDocker, "Basic"],
+      ["Android Studio", SiAndroidstudio, "Basic"],
+      ["Vercel", SiVercel, "Basic"],
+      ["Render", SiRender, "Basic"],
+    ],
+  ],
+];
